2551f7c5b1b259a9b2bf8b357299a7e247e0f245
Camila Alm <camila.salmeidatech@hotmail.com>
2024-12-05 18:28:45 -0500

    fix the time format

--- controllers/spots.js
+++ controllers/spots.js
@@ -21,16 +21,16 @@ router.post('/', async (req, res) => {
         const spot = await Spot.create(req.body);
         spot._doc.author = req.user;
         res.status(201).json(spot)
-    } catch(error) {
+    } catch (error) {
         res.status(500).json(error)
     }
 })
 
 router.get('/', async (req, res) => {
     try {
-        const spots = await Spot.find({}) 
-        .populate('author', 'username')                          // The populate() method in Mongoose is used to replace a reference field in a document (like an ID) with the actual data from the referenced document.
-        .sort({ createdAt: 'desc' })
+        const spots = await Spot.find({})
+            .populate('author', 'username')                          // The populate() method in Mongoose is used to replace a reference field in a document (like an ID) with the actual data from the referenced document.
+            .sort({ createdAt: 'desc' })
         //console.log('Populated Spots:', spots);
         res.status(200).json(spots)
     } catch (error) {
@@ -41,25 +41,25 @@ router.get('/', async (req, res) => {
 router.get('/:spotId', async (req, res) => {
     try {
         const spot = await Spot.findById(req.params.spotId)
-    .populate('author', 'username')
-    .populate('guests.author', 'username');
+            .populate('author', 'username')
+            .populate('guests.author', 'username');
         res.status(200).json(spot)
-    } catch (error){
+    } catch (error) {
         res.status(500).json(error)
     }
 })
 
 router.put('/:spotId', async (req, res) => {
-    try{
+    try {
         const spot = await Spot.findById(req.params.spotId)             // Retrive the spot we want to update from the database.
         if (!spot.author.equals(req.user._id)) {                         // Check if the user (this Id) has permission to update the resource.
-        return res.status(403).send('You are not allowed to Update this Spot!')
+            return res.status(403).send('You are not allowed to Update this Spot!')
         }
         const updatedSpot = await Spot.findByIdAndUpdate(                // Update Spot
-            req.params.spotId, 
-            req.body, 
+            req.params.spotId,
+            req.body,
             { new: true }
-        )                  
+        )
         updatedSpot._doc.author = req.user;                          // Apend the req.user to the author.
         res.status(200).json(updatedSpot)
     } catch (error) {
@@ -72,7 +72,7 @@ router.delete('/:spotId', async (req, res) => {
         const spot = await Spot.findById(req.params.spotId)                     // Find the SpotId. 
         if (!spot.author.equals(req.user._id)) {                                // Check the permissions.
             return res.status(403).send('You are not allowed to delete this Spot!.')
-        } 
+        }
         const deletedSpot = await Spot.findByIdAndDelete(req.params.spotId)     // Remove spot from the database.
         res.status(200).json(deletedSpot)
     } catch (error) {
@@ -86,11 +86,17 @@ router.post('/:spotId/guests', async (req, res) => {
     try {
         req.body.author = req.user._id                                          // Append the user to the author. 
         const spot = await Spot.findById(req.params.spotId)                     // Find the parent/spot Id.
+        // Formating the time
+        const timeFormatted = req.body.time
+            ? new Date(`1970-01-01T${req.body.time}`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })
+            : 'Not provided';
+        req.body.time = timeFormatted; // Overwrite time with formatted version
+
         spot.guests.push(req.body)                                              // We will use the push() method to add the new guest data to the guests array inside of the Spot document.
         await spot.save()                                                       // Save the guest to our database.
         const newGuest = spot.guests[spot.guests.length - 1]                    // Locate the new Guest using its position at the end of the the spot.guests array.
         newGuest._doc.author = req.user;                                        // Append the author property with a user object.
-        
+
         //Send email to the guest
         const subject = `You're Invited to ${spot.spotName}!`;
         const text = `Hi ${newGuest.name.toUpperCase()},
@@ -121,12 +127,12 @@ router.put('/:spotId/guests/:guestId', async (req, res) => {
         if (!spot) {
             return res.status(404).json({ message: 'Spot not found' });
         }
-        
+
         const guest = spot.guests.id(req.params.guestId)                        // Find the specific guest we wish to update within the array. To do so, we will use the MongooseDocumentArray(SPOT).prototype(GUEST).id()(ID(REQ.PARAMS.GUESTID)) method. This method is called on the array of a document, and returns an embedded subdocument based on the provided ObjectId ( req.params.guestId ).
         if (!guest) {
             return res.status(404).json({ message: 'Guest not found' });
         }
-        
+
         Object.assign(guest, req.body);
         console.log('Guest After Update:', guest);
         //guest.text = req.body.text                                              // Update its text property.
@@ -141,7 +147,7 @@ router.put('/:spotId/guests/:guestId', async (req, res) => {
 router.delete('/:spotId/guests/:guestId', async (req, res) => {
     try {
         const spot = await Spot.findById(req.params.spotId)                  // Find the parent.
-        spot.guests.remove({ _id: req.params.guestId })                      
+        spot.guests.remove({ _id: req.params.guestId })
         await spot.save()
         res.status(200).json({ message: 'Ok' })
     } catch (error) {
