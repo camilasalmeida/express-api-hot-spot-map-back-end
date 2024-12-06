dabc1a2fe22f1a1667657f99de1c378bf209b488
Camila Alm <camila.salmeidatech@hotmail.com>
2024-12-05 20:50:26 -0500

    final commit before tidying up the files

--- server.js
+++ server.js
@@ -17,16 +17,15 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/spots', spotsRouter);
-
-
-// Routes go here
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
-
 
 app.listen(3000, () => {
     console.log('The express app is ready!🍬');
